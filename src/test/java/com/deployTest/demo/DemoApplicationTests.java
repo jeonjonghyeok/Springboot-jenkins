5a8562a63686e02b1ca1fbb418c9e0bd0e03b52a
youminji <upi907@naver.com>
2021-09-26 20:54:33 +0900

    first commit

---
+++ src/test/java/com/deployTest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
